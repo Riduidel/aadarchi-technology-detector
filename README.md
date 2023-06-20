39b780c69ddb479d8ad8265d19dc620711f864c3
Helielzel <gaetanchaugny2@gmail.com>
2023-06-20 14:16:25 +0200

    readme update + requirements

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 <h1 align="center">Welcome to aadarchi-technology-detector 👋</h1>
 <p>
   <img alt="Version" src="https://img.shields.io/badge/version-1.00-blue.svg?cacheSeconds=2592000" />
-  <img src="https://img.shields.io/badge/npm-%3E%3D5.5.0-blue.svg" />
-  <img src="https://img.shields.io/badge/node-%3E%3D9.3.0-blue.svg" />
   <a href="https://github.com/kefranabg/readme-md-generator#readme" target="_blank">
     <img alt="Documentation" src="https://img.shields.io/badge/documentation-yes-brightgreen.svg" />
   </a>
--- mvn_tech_scraper/mvn_tech_scraper/spiders/mvnspider.py
+++ mvn_tech_scraper/mvn_tech_scraper/spiders/mvnspider.py
@@ -2,15 +2,37 @@ import scrapy
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
+#api key : 933ff5de-cd0f-4c71-ae93-ed565aefacfa
+
 class createMvnSPider(CrawlSpider):
     name = "mvnSpider"
     allowed_domains = "mvnrepository.com"
     start_url = "https://mvnrepository.com/"
 
     #spring example : mvnrepository.com/open-source/web-frameworks -> /artifact/org.springframework.boot/spring-boot-starter-web -> https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-web/3.1.0
+    # from mvn home page, all categories have "open-source" url section.
 
     rules = (
-        Rule(LinkExtractor(allow="artifact")),
-        Rule(LinkExtractor(allow="open-source"))
+        Rule(LinkExtractor(allow="open-source"), callback="parse_categories") #get categories
+        #Rule(LinkExtractor(allow="artifact")) -> get techs
         
     )
+
+    def parse_categories():
+        yield {
+
+        }
+
+
+
+# def parse(self, response):
+#         tags = response.css("a.post-tag") #on a div would be nice
+#         try:
+#             yield tags
+#         except:
+#             return
+        
+#         for tags in response.css("a.post-tag"):
+#             try:
+#                 'data': tags.css(::data // something to get the data field containing the tech name)
+#             yield:
