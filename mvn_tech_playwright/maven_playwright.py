013af0bcf47bc6cfb65653b50e5ef0efa4540ce8
Helielzel <gaetanchaugny2@gmail.com>
2023-06-28 11:43:02 +0200

    from html div to tech dictionary

--- mvn_tech_playwright/maven_playwright.py
+++ mvn_tech_playwright/maven_playwright.py
@@ -1,19 +1,25 @@
+#!/usr/bin/env python3
 from playwright.sync_api import sync_playwright
+import re
+
+def from_text_to_tech_dict(popular):
+    dico_tech = dict()
+    #print(popular)
+    for elem in popular:
+        if re.match('\d+\.\s', elem):
+            dico_tech[re.sub('\d+\.\s', "", elem)] = {}
+    print(dico_tech)
+
 
 def get_html_code():
-
     with sync_playwright() as plwr:
         browser = plwr.chromium.launch(headless=False)
         page = browser.new_page()
         page.goto("https://mvnrepository.com/popular")
-        html = page.inner_html("div.content")
-        print(html)
+        #page.wait_for_selector("div.content")
+        popular_techs = page.inner_text("div.content").split("\n")
+        from_text_to_tech_dict(popular_techs)
         browser.close()
-    
-    # div_element = page.query_selector("div#content")
-    # div_html = page.inner_html(div_element)
-    # browser.close()
-    # print(div_html)
 
 if __name__ == "__main__":
     get_html_code()
