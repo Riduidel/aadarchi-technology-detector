15d75e7313cfe3318a0271211cac3d4b90844c46
Nicolas Delsaux <Riduidel@users.noreply.github.com>
2023-09-29 13:42:16 +0200

    Fixes #7 (or at least the part that interest me) This allows TechEmpower benchmark to provide a list of interesting dependencies that may (or not) be available in maven central, and would be good technology candidates.

--- mvnrepository/extract_popular_mvnrepository_artifacts.py
+++ mvnrepository/extract_popular_mvnrepository_artifacts.py
@@ -2,11 +2,13 @@
 from playwright.sync_api import sync_playwright
 from playwright_stealth import stealth_sync
 from time import sleep
+import os
 import sys
 import re
 import json
 import logging
 import pathlib
+import subprocess
 parent_path = pathlib.Path(__file__).parent.resolve()
 
 logger = logging.getLogger('mvnrepository')
@@ -25,13 +27,79 @@ def load_javascript(javascript_file):
 
 SERVER = "mvnrepository.com"
 
+def identify_interesting_artifact_dependencies_in(framework_path):
+    '''
+    Load interesting dependencies of given pom.
+    You know how to do it the easiest way? By gently asking the dependencies maven plugin!
+    Unfortunatly, maven is unable to produce a machine-ready output
+    So we have to get the output in a file, then read back that file
+    '''
+    output_file = os.path.join(os.path.dirname(__file__),
+        ".cache", 
+        os.path.basename(framework_path)+".log")
+    from pathlib import Path
+    Path(output_file).parent.mkdir(parents=True, exist_ok=True)
+    command = "mvn dependency:list -DexcludeTransitive -DoutputFile={}".format(output_file)
+    logger.info("Will run command {} in folder {}".format(
+        command, framework_path))
+    if not output_file:
+        subprocess.run(command,
+            check=False,
+            shell=True,
+            cwd=framework_path)
+    returned = []
+    # Now the command has run, it is time to load the results
+    if os.path.isfile(output_file):
+        with open(output_file, 'r') as file:
+            command_result = file.read()
+            if command_result:
+                for dependency in re.findall("\s+([^:^\s]+):([^:^\s]+):", command_result):
+                    returned.append("https://{}/artifact/{}/{}".format(SERVER, dependency[0], dependency[1]))
+                    logger.info("found interesting dependency {}".format(dependency))
+    return returned
+
+def identify_interesting_artifacts_in_techempower():
+    '''
+    Identify interesting artifacts in **local** clone of techempower frameworks (git@github.com:TechEmpower/FrameworkBenchmarks.git)
+    For this to work optimally, the associated git repo must be in main branch
+    '''
+    interesting_artifacts_urls = []
+    frameworks_folder = "../FrameworkBenchmarks/frameworks"
+    files_and_dirs = os.listdir(frameworks_folder)
+    if not files_and_dirs:
+        logger.error("There must be a problem : frameworks folder {} seems to be empty".format(os.path.abspath(frameworks_folder)))
+    for language in files_and_dirs:
+        if language in ["Java"]:
+            language_full_path = frameworks_folder+"/"+language
+            logger.info("Now exploring {}".format(os.path.abspath(language)))
+            frameworks = os.listdir(language_full_path)
+            for framework_folder in frameworks:
+                framework_full_path = os.path.realpath(language_full_path + "/" + framework_folder)
+                if os.path.isdir(framework_full_path):
+                    logger.info("Now exploring {}".format(os.path.abspath(framework_full_path)))
+                    # Now we have a framework path, if it is has a maven pom, everything is cool
+                    if os.path.exists(framework_full_path+"/pom.xml"):
+                        interesting_artifacts_urls.extend(identify_interesting_artifact_dependencies_in(framework_full_path))
+                    else:
+                        logger.error("Pretty sure {} uses Gradle. How to parse infos in?")
+    return interesting_artifacts_urls
+
+def identify_interesting_artifacts(browser_tab):
+    interesting_artifacts_urls = []
+    interesting_artifacts_urls.extend(identify_interesting_artifacts_in_techempower())
+    return interesting_artifacts_urls
+
 def load_artifact(browser_tab, page_to_load):
     ''' Augment known details from artifact with all known versions, some artifact redirections, categories, and so on'''
-    logger.info("processing artifact %s", page_to_load)
-    browser_tab.goto(page_to_load)
-    # Now we're on artifact page, let's read all details
-    script = load_javascript("artifact_details_extractor.js")
-    return browser_tab.evaluate(script)
+    logger.debug("processing artifact %s", page_to_load)
+    response = browser_tab.goto(page_to_load)
+    if response.status<300:
+        # Now we're on artifact page, let's read all details
+        script = load_javascript("artifact_details_extractor.js")
+        return browser_tab.evaluate(script)
+    else:
+        logger.error("Unable to process artifact %s due to %s", page_to_load, response.status_text)
+        return None
 
 def locate_popular_artifacts(browser_tab, page_to_load="https://%s/popular"%(SERVER)):
     '''
@@ -55,6 +123,26 @@ def locate_popular_artifacts(browser_tab, page_to_load="https://%s/popular"%(SER
             logger.warning("The url %s is not complete during parsing lists. How is it even possible?", url)
     return popular_artifacts_urls
 
+def load_local_artifacts(browser_tab):
+    '''
+    Load a list of artifacts from a local file.
+    '''
+    if os.path.isfile(output_file):
+        with open(output_file, 'r') as file:
+            return file.read().splitlines()
+    return []
+
+def locate_interesting_artifacts(browser_tab):
+    '''
+    Combines both the mvn repository popular artifacts and some interesting projects 
+    (techempower benchmarks, medium clone, ...)
+    '''
+    interesting_artifacts_urls = []
+    interesting_artifacts_urls.extend(locate_popular_artifacts(browser_tab))
+    interesting_artifacts_urls.extend(identify_interesting_artifacts(browser_tab))
+    interesting_artifacts_urls.extend(load_local_artifacts(browser_tab))
+# Now we have a big list full of duplicates, dedup!
+    return list(set(interesting_artifacts_urls))
 
 def get_popular_artifacts():
     popular_artifacts = dict()
@@ -67,26 +155,29 @@ def get_popular_artifacts():
         context.add_init_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
         browser_tab = context.new_page()
         stealth_sync(browser_tab)
-        popular_artifacts_urls_reference = locate_popular_artifacts(browser_tab)
+        popular_artifacts_urls_reference = locate_interesting_artifacts(browser_tab)
         popular_artifacts_urls = popular_artifacts_urls_reference.copy()
         # Now we have the artifact urls, it's time to transform that into artifacts infos
         while popular_artifacts_urls:
             url = popular_artifacts_urls.pop()
+            if len(popular_artifacts_urls)%10==0:
+                logger.info("%d artifacts remaining.", len(popular_artifacts_urls))
             if not "mvnrepository" in url:
                 logger.warning("The url %s is not complete for detailled analysis. How is it even possible?", url)
             # For each artifact, extract more informations from artifact home page
             artifact_infos = load_artifact(browser_tab, url)
-            if "Central" in artifact_infos["repositories"]:
-                popular_artifacts[artifact_infos["coordinates"]] = artifact_infos
-            else:
-                logger.error("Maven central does not contains artifact %s. It will be ignored", artifact_infos["name"])
-            if "relocation" in artifact_infos:
-                if not artifact_infos["relocation"]["coordinates"] in popular_artifacts:
-                    if artifact_infos["relocation"]["coordinates"].endswith("."):
-                        logger.warning("artifact %s (see %s) has an invalid relocation definition, it will be ignored", 
-                            artifact_infos["name"], artifact_infos["page"])
-                    else:
-                        popular_artifacts_urls.append(artifact_infos["relocation"]["page"])
+            if artifact_infos:
+                if "Central" in artifact_infos["repositories"]:
+                    popular_artifacts[artifact_infos["coordinates"]] = artifact_infos
+                else:
+                    logger.error("Maven central does not contains artifact %s. It will be ignored", artifact_infos["name"])
+                if "relocation" in artifact_infos:
+                    if not artifact_infos["relocation"]["coordinates"] in popular_artifacts:
+                        if artifact_infos["relocation"]["coordinates"].endswith("."):
+                            logger.warning("artifact %s (see %s) has an invalid relocation definition, it will be ignored", 
+                                artifact_infos["name"], artifact_infos["page"])
+                        else:
+                            popular_artifacts_urls.append(artifact_infos["relocation"]["page"])
         browser.close()
         # Notice we don't get any version here!
         return dict(sorted(popular_artifacts.items()))
