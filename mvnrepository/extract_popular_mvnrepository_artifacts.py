1e57ff43f2a8161d97d8832c7e3a4be22d925b23
Nicolas Delsaux <Riduidel@users.noreply.github.com>
2023-10-02 09:04:49 +0200

    It was harder than expected, but it works well!

--- mvnrepository/extract_popular_mvnrepository_artifacts.py
+++ mvnrepository/extract_popular_mvnrepository_artifacts.py
@@ -22,8 +22,12 @@ ch.setFormatter(formatter)
 logger.addHandler(ch)
 
 SERVER = "mvnrepository.com"
+        
+def load_javascript(javascript_file):
+    with open("%s/%s"%(parent_path, javascript_file), 'r') as file:
+        return file.read()
 
-class ArtifactInformations:
+class ArtifactInformations(object):
     # The artifact group id. Should never be null
     groupId=None
     # The artifact id, which should never be null
@@ -34,6 +38,32 @@ class ArtifactInformations:
             self.groupId = kwargs['groupId']
         if 'artifactId' in kwargs:
             self.artifactId = kwargs['artifactId']
+
+    def __eq__(self, other):
+        """Overrides the default implementation"""
+        if isinstance(other, ArtifactInformations):
+            return self.groupId == other.groupId and self.artifactId == other.artifactId
+        return NotImplemented
+
+    def __ne__(self, other):
+        """Overrides the default implementation (unnecessary in Python 3)"""
+        x = self.__eq__(other)
+        if x is not NotImplemented:
+            return not x
+        return NotImplemented
+
+    def __hash__(self):
+        """Overrides the default implementation"""
+        return hash(tuple(sorted(self.__dict__.items())))
+    
+    def __str__(self):
+        return f"{self.groupId}:{self.artifactId}"
+    
+    def __repr__(self):
+        return f"ArtifactInformations<{self.groupId}:{self.artifactId}>"
+    def __lt__(a, b):
+        return a.__repr__()<b.__repr__()
+
     def to_json(self, browser_tab):
         ''' Augment known details from artifact with all known versions, some artifact redirections, categories, and so on'''
         page_to_load = f"https://{SERVER}/artifact/{self.groupId}/{self.artifactId}"
@@ -49,137 +79,153 @@ class ArtifactInformations:
                 else:
                     logger.error("Unable to process artifact %s due to %s", page_to_load, response.status_text)
                     return None
-            except Error:
+            except Exception:
                 tries = tries+1
 
-def load_javascript(javascript_file):
-    with open("%s/%s"%(parent_path, javascript_file), 'r') as file:
-        return file.read()
+class Loader:
+    def load(self):
+        logger.info("Unable to run loader %s", type(self).__name__)
+    def load_page_list(self, page_to_load):
+        has_next_page = True
+        script = load_javascript("artifacts_urls_extractor.js")
+        popular_artifacts_urls = []
+        while page_to_load:
+            logger.info("Loading page %s"%page_to_load)
+            self.browser_tab.goto(page_to_load)
+            page_infos = self.browser_tab.evaluate(script)
+            if not page_infos["data"]:
+                raise ValueError(f"Page {page_to_load} has empty data {page_infos}")
+            for artifact in page_infos["data"]:
+                popular_artifacts_urls.append(ArtifactInformations(**artifact))
+            if "next" in page_infos["page"] and page_infos["page"]["next"]:
+                page_to_load = page_infos["page"]["next"]
+            else:
+                page_to_load = None
+        return popular_artifacts_urls
 
-def identify_interesting_artifact_dependencies_in_gradle_project(framework_path):
-    logger.error("TODO implement gradle project analysis for %s", framework_path)
-    return []
 
-def identify_interesting_artifact_dependencies_in_maven_project(framework_path):
-    '''
-    Load interesting dependencies of given pom.
-    You know how to do it the easiest way? By gently asking the dependencies maven plugin!
-    Unfortunatly, maven is unable to produce a machine-ready output
-    So we have to get the output in a file, then read back that file
-    '''
-    output_file = os.path.join(os.path.dirname(__file__),
-        ".cache", 
-        os.path.basename(framework_path)+".log")
-    from pathlib import Path
-    Path(output_file).parent.mkdir(parents=True, exist_ok=True)
-    command = "mvn dependency:list -DexcludeTransitive -DoutputFile={}".format(output_file)
-    logger.info("Will run command {} in folder {}".format(
-        command, framework_path))
-    if not os.path.isfile(output_file):
-        def run_in_maven(command):
-            return subprocess.run(command,
-                check=False,
-                shell=True,
-                cwd=framework_path)
-        execution_result = run_in_maven(command)
-        if execution_result.returncode!=0:
-            # We will try to install the project, then rerun the command
-            run_in_maven("mvn install")
-            # Then rerun command (and hope for the result to be ok)
+class LocalFile(Loader):
+    def __init__(self, browser_tab):
+        self.browser_tab = browser_tab
+    def load(self):
+        '''
+        Load a list of artifacts from a local file.
+        '''
+        interesting_local_artifacts = os.path.join(os.path.dirname(__file__), "interesting_artifacts.json")
+        returned = []
+        if os.path.isfile(interesting_local_artifacts):
+            with open(interesting_local_artifacts, 'r') as file:
+                file_contents = file.read()
+                artifacts = json.loads(file_contents)
+                for a in artifacts:
+                    if "artifactId" in a:
+                        returned.append(ArtifactInformations(**a))
+                    else:
+                        groupId = a["groupId"]
+                        artifacts_of_group = self.load_page_list(f"https://{SERVER}/artifact/{groupId}")
+                        returned.extend(artifacts_of_group)
+        return returned
+
+class TechEmpower(Loader):
+    def __init__(self, browser_tab):
+        self.browser_tab = browser_tab
+        
+    def identify_interesting_artifact_dependencies_in_gradle_project(self, framework_path):
+        logger.error("TODO implement gradle project analysis for %s", framework_path)
+        return []
+
+    def identify_interesting_artifact_dependencies_in_maven_project(self, framework_path):
+        '''
+        Load interesting dependencies of given pom.
+        You know how to do it the easiest way? By gently asking the dependencies maven plugin!
+        Unfortunatly, maven is unable to produce a machine-ready output
+        So we have to get the output in a file, then read back that file
+        '''
+        output_file = os.path.join(os.path.dirname(__file__),
+            ".cache", 
+            os.path.basename(framework_path)+".log")
+        from pathlib import Path
+        Path(output_file).parent.mkdir(parents=True, exist_ok=True)
+        command = "mvn dependency:list -DexcludeTransitive -DoutputFile={}".format(output_file)
+        logger.info("Will run command {} in folder {}".format(
+            command, framework_path))
+        if not os.path.isfile(output_file):
+            def run_in_maven(command):
+                return subprocess.run(command,
+                    check=False,
+                    shell=True,
+                    cwd=framework_path)
             execution_result = run_in_maven(command)
             if execution_result.returncode!=0:
-                logger.error("Unable to compile %s to have valid dependencies", framework_path)
-    returned = []
-    # Now the command has run, it is time to load the results
-    if os.path.isfile(output_file):
-        with open(output_file, 'r') as file:
-            command_result = file.read()
-            if command_result:
-                for dependency in re.findall("\s+([^:^\s]+):([^:^\s]+):", command_result):
-                    returned.append(ArtifactInformations(groupId=dependency[0], artifactId=dependency[1]))
-                    logger.info("found interesting dependency {}".format(dependency))
-    return returned
-
-def find_frameworks_folder_in(folder):
-    '''
-    If the given folder contains the FrameworkBenchmarks/frameworks folder, return it.
-    Otherwise test with parent.
-    If folder is null, throw a ValueError
-    '''
-    returned = os.path.join(folder,"FrameworkBenchmarks/frameworks")
-    if os.path.isdir(returned):
+                # We will try to install the project, then rerun the command
+                run_in_maven("mvn install")
+                # Then rerun command (and hope for the result to be ok)
+                execution_result = run_in_maven(command)
+                if execution_result.returncode!=0:
+                    logger.error("Unable to compile %s to have valid dependencies", framework_path)
+        returned = []
+        # Now the command has run, it is time to load the results
+        if os.path.isfile(output_file):
+            with open(output_file, 'r') as file:
+                command_result = file.read()
+                if command_result:
+                    for dependency in re.findall("\s+([^:^\s]+):([^:^\s]+):", command_result):
+                        returned.append(ArtifactInformations(groupId=dependency[0], artifactId=dependency[1]))
+                        logger.info("found interesting dependency {}".format(dependency))
         return returned
-    elif not folder:
-        raise ValueError("couldn't find framewok folder")
-    else:
-        return find_frameworks_folder_in(os.path.dirname(folder))
 
-
-def identify_interesting_artifacts_in_techempower():
-    '''
-    Identify interesting artifacts in **local** clone of techempower frameworks (git@github.com:TechEmpower/FrameworkBenchmarks.git)
-    For this to work optimally, the associated git repo must be in main branch
-    '''
-    interesting_artifacts_urls = []
-    frameworks_folder = find_frameworks_folder_in(os.path.dirname(__file__))
-    files_and_dirs = os.listdir(frameworks_folder)
-    if not files_and_dirs:
-        logger.error("There must be a problem : frameworks folder {} seems to be empty".format(os.path.abspath(frameworks_folder)))
-    for language in files_and_dirs:
-        if language in ["Java"]:
-            language_full_path = frameworks_folder+"/"+language
-            logger.info("Now exploring {}".format(os.path.abspath(language)))
-            frameworks = os.listdir(language_full_path)
-            for framework_folder in frameworks:
-                framework_full_path = os.path.realpath(language_full_path + "/" + framework_folder)
-                if os.path.isdir(framework_full_path):
-                    logger.info("Now exploring {}".format(os.path.abspath(framework_full_path)))
-                    # Now we have a framework path, if it is has a maven pom, everything is cool
-                    if os.path.exists(framework_full_path+"/pom.xml"):
-                        interesting_artifacts_urls.extend(identify_interesting_artifact_dependencies_in_maven_project(framework_full_path))
-                    elif os.path.exists(framework_full_path+"/build.gradle"):
-                        interesting_artifacts_urls.extend(identify_interesting_artifact_dependencies_in_gradle_project(framework_full_path))
-                    else:
-                        logger.error("Pretty sure %s uses Gradle. How to parse infos in?", framework_full_path)
-    return interesting_artifacts_urls
-
-def identify_interesting_artifacts(browser_tab):
-    interesting_artifacts_urls = []
-    interesting_artifacts_urls.extend(identify_interesting_artifacts_in_techempower())
-    return interesting_artifacts_urls
-
-def locate_popular_artifacts(browser_tab, page_to_load="https://%s/popular"%(SERVER)):
-    '''
-    To get popular artifacts, we start from an initial page and navigate the next button as long as it is not grayed out.
-    This method returns a list of **absolute** links to artifacts pages.
-    '''
-    has_next_page = True
-    script = load_javascript("artifacts_urls_extractor.js")
-    popular_artifacts_urls = []
-    while page_to_load:
-        logger.info("Loading page %s"%page_to_load)
-        browser_tab.goto(page_to_load)
-        page_infos = browser_tab.evaluate(script)
-        for artifact in page_infos["data"]:
-            popular_artifacts_urls.append(ArtifactInformations(**artifact))
-        if "next" in page_infos["page"] and page_infos["page"]["next"]:
-            page_to_load = page_infos["page"]["next"]
+    def find_frameworks_folder_in(self, folder):
+        '''
+        If the given folder contains the FrameworkBenchmarks/frameworks folder, return it.
+        Otherwise test with parent.
+        If folder is null, throw a ValueError
+        '''
+        returned = os.path.join(folder,"FrameworkBenchmarks/frameworks")
+        if os.path.isdir(returned):
+            return returned
+        elif not folder:
+            raise ValueError("couldn't find framewok folder")
         else:
-            page_to_load = None
-    return popular_artifacts_urls
+            return self.find_frameworks_folder_in(os.path.dirname(folder))
 
-def load_local_artifacts(browser_tab):
-    '''
-    Load a list of artifacts from a local file.
-    '''
-    interesting_local_artifacts = os.path.join(os.path.dirname(__file__), "interesting_artifacts.txt")
-    if os.path.isfile(interesting_local_artifacts):
-        with open(interesting_local_artifacts, 'r') as file:
-            returned = []
-            file_contents = file.read()
-            for dependency in re.findall("artifact/([^/]+)/([^/]+)/", file_contents):
-                returned.append(ArtifactInformations(groupId=dependency[0], artifactId=dependency[1]))
-    return []
+
+    def load(self):
+        '''
+        Identify interesting artifacts in **local** clone of techempower frameworks (git@github.com:TechEmpower/FrameworkBenchmarks.git)
+        For this to work optimally, the associated git repo must be in main branch
+        '''
+        interesting_artifacts_urls = []
+        frameworks_folder = self.find_frameworks_folder_in(os.path.dirname(__file__))
+        files_and_dirs = os.listdir(frameworks_folder)
+        if not files_and_dirs:
+            logger.error("There must be a problem : frameworks folder {} seems to be empty".format(os.path.abspath(frameworks_folder)))
+        for language in files_and_dirs:
+            if language in ["Java"]:
+                language_full_path = frameworks_folder+"/"+language
+                logger.info("Now exploring {}".format(os.path.abspath(language)))
+                frameworks = os.listdir(language_full_path)
+                for framework_folder in frameworks:
+                    framework_full_path = os.path.realpath(language_full_path + "/" + framework_folder)
+                    if os.path.isdir(framework_full_path):
+                        logger.info("Now exploring {}".format(os.path.abspath(framework_full_path)))
+                        # Now we have a framework path, if it is has a maven pom, everything is cool
+                        if os.path.exists(framework_full_path+"/pom.xml"):
+                            interesting_artifacts_urls.extend(self.identify_interesting_artifact_dependencies_in_maven_project(framework_full_path))
+                        elif os.path.exists(framework_full_path+"/build.gradle"):
+                            interesting_artifacts_urls.extend(self.identify_interesting_artifact_dependencies_in_gradle_project(framework_full_path))
+                        else:
+                            logger.error("Pretty sure %s uses Gradle. How to parse infos in?", framework_full_path)
+        return interesting_artifacts_urls
+
+class Popular(Loader):
+    def __init__(self, browser_tab):
+        self.browser_tab = browser_tab
+    def load(self, page_to_load="https://%s/popular"%(SERVER)):
+        '''
+        To get popular artifacts, we start from an initial page and navigate the next button as long as it is not grayed out.
+        This method returns a list of **absolute** links to artifacts pages.
+        '''
+        return self.load_page_list(page_to_load)
 
 def locate_interesting_artifacts(browser_tab):
     '''
@@ -189,13 +235,15 @@ def locate_interesting_artifacts(browser_tab):
     returns a list of ArtifactInformations
     '''
     interesting_artifacts_urls = []
-    interesting_artifacts_urls.extend(load_local_artifacts(browser_tab))
-    interesting_artifacts_urls.extend(identify_interesting_artifacts(browser_tab))
-    interesting_artifacts_urls.extend(locate_popular_artifacts(browser_tab))
+    for loader in [LocalFile(browser_tab),
+        TechEmpower(browser_tab),
+        Popular(browser_tab)]:
+        interesting_artifacts_urls.extend(loader.load())
 # Now we have a big list full of duplicates, dedup!
-    return list(set(interesting_artifacts_urls))
+    return sorted(set(interesting_artifacts_urls))
 
 def get_popular_artifacts():
+    '''main method that will produce a dict of intersting rtifacts'''
     popular_artifacts = dict()
 
     with sync_playwright() as plwr:
