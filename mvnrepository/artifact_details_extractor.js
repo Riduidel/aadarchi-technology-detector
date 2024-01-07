4ad1484e838d4bae9f09eff1fe1f817050cdfa74
Nicolas Delsaux <nicolas.delsaux@gmx.fr>
2024-01-07 11:40:49 +0100

    Fixed some weird quirks

--- mvnrepository/artifact_details_extractor.js
+++ mvnrepository/artifact_details_extractor.js
@@ -28,12 +28,18 @@ function extract_artifact_details() {
                 const firstColum = tr.children[0]
                 var returned = {}
                 for(var property in this) {
-                    if(!property.startsWith("_") && property!="repository" && property!="vulnerabilities" && property!="") {
-                        // If the empty column exists, we have version grouping
+                    if(!property.startsWith("_") && !["repository", "vulnerabilities", ""].includes(property)) {
+//                        console.debug("reading row", tr, "property", property, "mapping is", this)
+                        // The "" property is set by the block setting _VERSION_COLUMN_NAME
+                        // (see the "setting _VERSION_COLUMN_NAME" comment).
+                        // So when the "" property is set, it means there is an empty column, which
+                        // is linked to version grouping (1.1.x and so on)
                         if(this.hasOwnProperty("")) {
                             if(firstColum.getAttribute("rowspan")) {
                                 returned[property] = tr.children[this[property]].innerText
-                            } else {
+                                // If there is no rownspan in first column, we're not on a row grouping successive rows
+                                // So only handle the 1+ columns
+                            } else if(this[property]>0) {
                                 returned[property] = tr.children[this[property]-1].innerText
                             }
                         } else {
@@ -46,6 +52,7 @@ function extract_artifact_details() {
                     returned["users"]=parseUsageString(returned["usages"])
 //                    delete returned["usages"]
                 }
+//                console.log("Reading row", tr, "using properties", this, "resulted into", returned)
                 return returned
             },
             "_rowToKeyValue": function(tr) {
@@ -56,14 +63,33 @@ function extract_artifact_details() {
                 return [version, attributes]
             }
         }
-        Array.from(versionsTable.querySelectorAll("th"))
-            .forEach((th, index) => {
-                const text = th.innerText.toLowerCase()
-                headersToCols[text] = index
-                if(text.startsWith("version")) {
-                    headersToCols._VERSION_COLUMN_NAME=text
-                }
+        // setting _VERSION_COLUMN_NAME
+        // The initial version didn't worked when header celles were set on two rows and the second included some artifact
+        // So time to go the long way
+        var headers = {}
+        // That offset will only work when split cell is at the end, otherwise it will be such a fucked up mess!
+        var offset = 0
+        Array.from(versionsTable.querySelectorAll("tr"))
+            .forEach(tr => {
+                rowOffset = 0
+                Array.from(tr.querySelectorAll("th"))
+                    .forEach((th, index) => {
+                                const text = th.innerText.toLowerCase()
+                                headers[index+offset]=text
+                                if(!headersToCols.hasOwnProperty("_VERSION_COLUMN_NAME")) {
+                                    if(text.startsWith("version")) {
+                                        headersToCols._VERSION_COLUMN_NAME=text
+                                    }
+                                }
+                                if(th.getAttribute("rowspan")) {
+                                    rowOffset+=1
+                                }
+                            })
+                offset = rowOffset
             })
+        for(var headerIndex in headers) {
+            headersToCols[headers[headerIndex]]=headerIndex
+        }
         artifact["versions"] = Array.from(versionsTable.querySelectorAll("tr"))
             .filter(element => element.querySelector("td"))
             .map(tr => headersToCols._rowToKeyValue(tr))
@@ -79,6 +105,13 @@ function extract_artifact_details() {
                 if(element.tagName.toLowerCase()=="table") {
                     readWhateverVersionTable(element, artifact)
                     return null
+                }
+                if(element.tagName.toLowerCase()=="div") {
+                    const includedTable = element.querySelector("table")
+                    if(includedTable) {
+                        readWhateverVersionTable(includedTable, artifact)
+                        return null
+                    }
                 }
             }
             return read2009Versions
@@ -119,6 +152,85 @@ function extract_artifact_details() {
             return read2009Name
         }
         return read2009Name
+    }
+    function get2014StartCallback() {
+        const read2014Versions =  function(element, artifact) {
+            // in that particular case, there are two measures of an artifact usage : artifacts and versions
+            // here we choose to copy the "versions" into the "usage" and "users" fields for consistency
+            function fixVersions(artifact) {
+                for(var v in artifact.versions) {
+                    var historicVersion = artifact.versions[v]
+                    if(historicVersion.hasOwnProperty("versions"))
+                        historicVersion.usages = historicVersion.versions
+                    if(historicVersion.hasOwnProperty("usages")) 
+                        historicVersion.users = parseUsageString(historicVersion.usages)
+                }
+            }
+            console.debug("Using ", arguments.callee.name, " for ", element, "to fill", artifact)
+            if(element.nodeType==1) {
+                if(element.tagName.toLowerCase()=="table") {
+                    readWhateverVersionTable(element, artifact)
+                    return null
+                }
+                if(element.tagName.toLowerCase()=="div") {
+                    if(element.tagName.toLowerCase()=="table") {
+                        readWhateverVersionTable(element, artifact)
+                        fixVersions(artifact)
+                        return null
+                    }
+                    if(element.tagName.toLowerCase()=="div") {
+                        const includedTable = element.querySelector("table")
+                        if(includedTable) {
+                            readWhateverVersionTable(includedTable, artifact)
+                            fixVersions(artifact)
+                            return null
+                        }
+                    }
+                }
+            }
+            return read2014Versions
+        }
+        const read2014Tags = function(element, artifact) {
+            if(!storage.hasOwnProperty(arguments.callee.name)) {
+                storage[arguments.callee.name]={"inTags": false}
+            }
+            console.debug("Using ", arguments.callee.name, " for ", element, "to fill", artifact)
+            if(element.nodeType==1) {
+                if(element.tagName.toLowerCase()=="h3") {
+                    return read2014Versions
+                } else if(element.tagName.toLowerCase()=="a") {
+                    if(storage[arguments.callee.name].inTags) {
+                        if(!artifact["tags"]) {
+                            artifact["tags"]=[]
+                        }
+                        artifact["tags"].push(element.innerText)
+                    }
+                }
+            } else if(element.nodeType==3) {
+                if(element.nodeValue.includes("tags")) {
+                    storage[arguments.callee.name].inTags = true
+                }
+            }
+            return read2014Tags
+        }
+        const read2014Name = function(element, artifact) {
+            console.debug("Using ", arguments.callee.name, " for ", element, "to fill", artifact)
+            if(element.nodeType==1) {
+                if(element.tagName.toLowerCase()=="div") {
+                    if(element.getAttribute("class")=="breadcrumb") {
+
+                    } else {
+                        var header = element.querySelector("h3")
+                        artifact["name"] = header.childNodes[0].textContent
+                    }
+                } else if(element.tagName.toLowerCase()=="br") {
+                    artifact["description"]=element.innerText
+                    return read2014Versions;
+                }
+            }
+            return read2014Name
+        }
+        return read2014Name
     }
     function get2023StartCallback() {
         const read2023Versions =  function(element, artifact) {
@@ -185,18 +297,22 @@ function extract_artifact_details() {
                                 } else {
                                     console.log("no match found in text "+value_cell)
                                 }
-                    
+                                break;
                             case "Used By":
                                 const usages = tr.children[1].innerText
-                                if(!isNaN(usages)) {
-                                    artifact["users"] = parseUsageString(usages)
-                                }
+                                artifact["users"] = parseUsageString(usages)
                                 break;
                             default:
                                 break;
                         }
                     })
                     return read2023Relocation
+                } else if(element.tagName.toLowerCase()=="div") {
+                    if(element.querySelector("table.versions")) {
+                        // Oh fuck, we missed something, let's jump directly to versions table
+                        read2023Versions(element, artifact)
+                        return null
+                    }
                 }
             }
             return read2023LicenseCategoryTable
@@ -224,9 +340,7 @@ function extract_artifact_details() {
                         }
                         if(element.querySelector(".im-usage")) {
                             const usages = element.querySelector(".im-usage").innerText
-                            if(!isNaN(usages)) {
-                                artifact["users"] = parseUsageString(usages)
-                            }
+                            artifact["users"] = parseUsageString(usages)
                         }
                         if(element.querySelector("h2 a")) {
                             artifact["name"] = element.querySelector("h2 a").innerText
@@ -264,11 +378,17 @@ function extract_artifact_details() {
         analyzedElement = document.querySelector("div#maincontent")
         // Wait, we need more fine tuning
         if(analyzedElement.querySelector("div.breadcrumb")) {
-            console.log("Selected MvnRepository 2021")
-            usedCallback = get2023StartCallback()
+            if(analyzedElement.querySelector("div.im")) {
+                console.log("Selected MvnRepository 2021")
+                usedCallback = get2023StartCallback()
+            } else {
+                // Can be tested on https://web.archive.org/web/20140901020518/http://mvnrepository.com/artifact/ch.qos.logback/logback-classic/
+                console.log("Selected MvnRepository autumn 2014")
+                usedCallback = get2014StartCallback()
+            }
         } else {
             // Can be tested on https://web.archive.org/web/20140209041351/http://mvnrepository.com/artifact/ch.qos.logback/logback-classic
-            console.log("Selected MvnRepository 2014")
+            console.log("Selected MvnRepository spring 2014")
             usedCallback = get2009StartCallback()
         }
     } else if(document.querySelector("div#body")) {
@@ -280,8 +400,11 @@ function extract_artifact_details() {
         throw "Can't read artifact informations from "+document.location
     }
     Array.from(analyzedElement.childNodes).forEach(element => {
-        if(usedCallback)
+        if(usedCallback) {
+            // Addedto make  debugging simpler
+            artifact = structuredClone(artifact)
             usedCallback = usedCallback(element, artifact)
+        }
     })
     return artifact
 }
