cbfb0009c92bd73a79466bf42add8ddbdaf5e8b1
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2024-04-18 17:21:02 +0200

    feat: :lipstick: better ux on cli

--- githubCrawler/main.ts
+++ githubCrawler/main.ts
@@ -8,7 +8,11 @@ import { top1000 as topPHPPackages } from "./packagistorg/top1000";
 import { top1000 as topJSPackages } from "./npmjscom/top1000";
 import { saveJson } from "./utils";
 
-//const topPHP = await topPHPPackages();
+console.log("Start analysis");
+console.log("🔍 Top 1000 PHP packages 🐘");
+const topPHP = await topPHPPackages();
+
+console.log("🔍 Top 1000 JS packages 🟡");
 const topJS = await topJSPackages();
 
-saveJson("outputs/artifact.json", { ...topJS });
+saveJson("outputs/artifact.json", { ...topJS, ...topPHP });
