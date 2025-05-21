597b3c306cad2d85f227d928054b85ed191ec033
Nicolas Delsaux <nicolas.delsaux@gmx.fr>
2025-05-21 09:52:45 +0200

    ✨ Fixes #133 (#135)
    Allow github indicators to be skipped when tokens are no more available.
    We may need to inform Camel of the next available window somehow ...

--- src/main/java/org/ndx/aadarchi/technology/detector/indicators/IndicatorComputer.java
+++ src/main/java/org/ndx/aadarchi/technology/detector/indicators/IndicatorComputer.java
@@ -1,5 +1,14 @@
 package org.ndx.aadarchi.technology.detector.indicators;
 
+import org.ndx.aadarchi.technology.detector.model.Technology;
+
 public interface IndicatorComputer {
 	String getFromRouteName();
+
+	/**
+	 * Simply says if computer can compute technology. We may need more details later ...
+	 * @param technology
+	 * @return true when indicator can compute value for technology.
+	 */
+	boolean canCompute(Technology technology);
 }
