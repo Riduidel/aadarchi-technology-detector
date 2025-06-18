12c3925b14b6885608e4b6f1a806464a29209355
Nicolas Delsaux <nicolas.delsaux@gmx.fr>
2025-06-18 11:52:48 +0200

    It will really be time to provide some non-code configuration of starter route (maybe even a UI to enable/disable things by hand)

--- src/main/java/org/ndx/aadarchi/technology/detector/StarterRoute.java
+++ src/main/java/org/ndx/aadarchi/technology/detector/StarterRoute.java
@@ -1,6 +1,8 @@
 package org.ndx.aadarchi.technology.detector;
 
 import org.apache.camel.builder.endpoint.EndpointRouteBuilder;
+import org.ndx.aadarchi.technology.detector.export.csv.ExportToCsv;
+import org.ndx.aadarchi.technology.detector.export.json.ExportToJson;
 
 public class StarterRoute extends EndpointRouteBuilder {
 	
@@ -9,10 +11,11 @@ public class StarterRoute extends EndpointRouteBuilder {
 		from(timer("autostart").repeatCount(1))
 			.id("1-starter-route")
 			.log("Starting the whole process")
-			.to(direct(ReadPopularLibraries.class.getSimpleName()))
-			.to(direct(GenerateIndicatorComputations.class.getSimpleName()))
-			.to(direct(ProcessIndicatorComputations.class.getSimpleName()))
-			.to(direct(ExportDatabase.class.getSimpleName()))
+//			.to(direct(ReadPopularLibraries.class.getSimpleName()))
+//			.to(direct(GenerateIndicatorComputations.class.getSimpleName()))
+//			.to(direct(ProcessIndicatorComputations.class.getSimpleName()))
+//			.to(direct(ExportToJson.class.getSimpleName()))
+			.to(direct(ExportToCsv.class.getSimpleName()))
 			.log("Everything should be terminated now.")
 			;
 	}
