82a1c6c489fa7c0daa0fc58a7f5815b6c6ce78d6
Nicolas Delsaux <nicolas.delsaux@gmx.fr>
2025-09-09 14:40:54 +0200

    This should ahve been commited long ago!

---
+++ src/test/java/org/ndx/aadarchi/technology/detector/indicators/github/graphql/GitHubGraphqlFacadeTest.java
@@ -0,0 +1,49 @@
+package org.ndx.aadarchi.technology.detector.indicators.github.graphql;
+
+import static org.assertj.core.api.Assertions.from;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.apache.camel.RoutesBuilder;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.quarkus.test.CamelQuarkusTestSupport;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+
+@QuarkusTest
+class GitHubGraphqlFacadeTest extends CamelQuarkusTestSupport{
+	@Inject GitHubGraphqlFacade facade;
+	
+	@Override
+	protected RoutesBuilder createRouteBuilder() throws Exception {
+		return new RouteBuilder() {
+			@Override
+			public void configure() throws Exception {
+				from("direct:start").transform().simple("Hello ${body}").to("mock:result");
+			}
+		};
+	}
+
+	@Test
+	void can_count_forks_for_one_popular_project() {
+		Assertions.assertThat(facade.getCurrentTotalNumberOfFork("microsoft", "TypeScript"))
+			.isGreaterThan(100);
+	}
+
+
+	@Test
+	void can_get_more_than_100_forks_for_one_popular_project() {
+		// Given
+		AtomicLong forkCount = new AtomicLong();
+		// When
+		facade.getAllForks("microsoft", "TypeScript", false, forkList -> {
+			forkCount.addAndGet(forkList.forks.nodes.size());
+			return forkCount.longValue()<=200;
+		});
+		// Then
+		Assertions.assertThat(forkCount.longValue()).isGreaterThan(100);
+	}
+}
