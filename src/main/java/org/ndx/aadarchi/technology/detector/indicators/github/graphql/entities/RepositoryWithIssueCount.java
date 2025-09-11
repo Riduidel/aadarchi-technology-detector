25b2da2b3d548c301dde59e7912d3966a89a6223
guillaume.elhadi <guillaume.elhadi@gmail.com>
2025-09-11 18:09:59 +0200

    Feat/147: Add Issue count indicator

---
+++ src/main/java/org/ndx/aadarchi/technology/detector/indicators/github/graphql/entities/RepositoryWithIssueCount.java
@@ -0,0 +1,13 @@
+package org.ndx.aadarchi.technology.detector.indicators.github.graphql.entities;
+
+/*
+* DTO for the response of the query for the total number of issues
+* */
+public class RepositoryWithIssueCount {
+
+    public Issues issues;
+
+    public static class Issues {
+        public int totalCount;
+    }
+}
---
+++ src/main/java/org/ndx/aadarchi/technology/detector/indicators/github/graphql/entities/RepositoryWithIssueList.java
@@ -0,0 +1,30 @@
+package org.ndx.aadarchi.technology.detector.indicators.github.graphql.entities;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+
+/*
+* DTO for the fork history query response
+* */
+public class RepositoryWithIssueList {
+    public Issues issues;
+
+    public static class Issues {
+        public List<IssueNode> nodes;
+        public PageInfo pageInfo;
+    }
+
+    public static class IssueNode {
+        public OffsetDateTime createdAt;
+        public IssueAuthor owner;
+    }
+
+    public static class IssueAuthor {
+        public String login;
+    }
+
+    public static class PageInfo {
+        public boolean hasPreviousPage;
+        public String startCursor;
+    }
+}
