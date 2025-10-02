0258f6ed79c3cb135203ff6fbf369fb5f9caf879
Nicolas Delsaux <nicolas.delsaux@gmx.fr>
2025-10-02 10:01:14 +0200

    Some niceties before rewriting the whole stuff

--- src/build/groovy/updateJacocoTarget.groovy
+++ src/build/groovy/updateJacocoTarget.groovy
@@ -3,6 +3,8 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import groovy.xml.*
+
 def PROPERTY = "jacoco.covered.instructions.target.percentage"
 File jacocoReport = new File(basedir, "/target/site/jacoco/index.html")
 log.info "Reading effective coverage from ${jacocoReport.absolutePath}"
@@ -22,16 +24,21 @@ if(effectiveJacocoLevel>initialJacocoLevel) {
 	def coordinates = "${groupId}:${artifactId}"
 	log.info "Parsed POM of ${coordinates}"
 	if(projectXml.properties.size()==0) {
-		log.warn("There are no <properties/> defined in ${coordinates}. Appending them at the end")
+		log.warn "There are no <properties/> defined in ${coordinates}. Appending them"
 		projectXml.append(new NodeBuilder().properties())
 	}
 	def properties = projectXml.properties[0]
 	// Do we have the good property in?
 	def maybeJacocoProperty = properties.findAll { PROPERTY.equals(it.name().localPart) }
 	if(maybeJacocoProperty.size()==0) {
+		log.warn "There is no <${PROPERTY}/> defined in <properties/> of ${coordinates}. Appending it"
 		properties.append(new NodeBuilder()."${PROPERTY}"())
 	}
 	def jacocoProperty = properties.findAll { PROPERTY.equals(it.name().localPart) }.first()
+	def jacocoStringValue = String.format(Locale.US, "%1.2f", effectiveJacocoLevel)
+	jacocoProperty.value = jacocoStringValue
+	log.info "Updated <${PROPERTY}/> value to ${jacocoStringValue}"
+
 	jacocoProperty.value = String.format(Locale.US, "%1.2f", effectiveJacocoLevel)
 	def printer = new XmlNodePrinter(new PrintWriter(new FileWriter(pomFile)))
 	printer.preserveWhitespace = true
