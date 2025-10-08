defda224ec9ac875ec28ebed9b351e0833bacc00
Nicolas Delsaux <nicolas.delsaux@gmx.fr>
2025-10-08 17:05:36 +0200

    Pretty sure tests should work in CI with that

--- src/test/java/com/zenika/tech/lab/ingester/processors/IndicatorComputationProcessorTest.java
+++ src/test/java/com/zenika/tech/lab/ingester/processors/IndicatorComputationProcessorTest.java
@@ -1,7 +1,5 @@
 package com.zenika.tech.lab.ingester.processors;
 
-import static org.assertj.core.api.Assertions.from;
-
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
@@ -11,11 +9,15 @@ import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.quarkus.test.CamelQuarkusTestSupport;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import com.zenika.tech.lab.ingester.indicators.IndicatorComputer;
 import com.zenika.tech.lab.ingester.model.IndicatorComputation;
+import com.zenika.tech.lab.ingester.model.IndicatorComputationBuilder;
+import com.zenika.tech.lab.ingester.model.IndicatorComputationRepository;
+import com.zenika.tech.lab.ingester.model.Technology;
+import com.zenika.tech.lab.ingester.model.TechnologyBuilder;
+import com.zenika.tech.lab.ingester.model.TechnologyRepository;
 
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.enterprise.inject.Instance;
@@ -25,6 +27,8 @@ import jakarta.transaction.Transactional;
 @QuarkusTest
 class IndicatorComputationProcessorTest extends CamelQuarkusTestSupport {
 	
+	@Inject TechnologyRepository technologies;
+	@Inject IndicatorComputationRepository indicatorComputations;
 	@Inject IndicatorComputationProcessor tested;
 	@Inject Instance<IndicatorComputer> indicatorComputers;
 	private long INDICATORS_COUNT;
@@ -48,6 +52,28 @@ class IndicatorComputationProcessorTest extends CamelQuarkusTestSupport {
 	@Test @Transactional
 	void find_all_priorized_should_return_react_first() {
 		// Given
+		if(indicatorComputations.count()==0) {
+			// Create some test data when none exist
+			// This is required since we tend to abuse dev mode
+			Technology react = TechnologyBuilder.technology()
+					.platform("NPM")
+					.name("react")
+					.build();
+			Technology everything = TechnologyBuilder.technology()
+					.platform("NPM")
+					.name("everything")
+					.build();
+			technologies.persist(react, everything);
+			indicatorComputations.persist(
+				IndicatorComputationBuilder.indicatorComputation()
+					.technology(react)
+					.indicatorRoute("log:just a test")
+					.build(),
+				IndicatorComputationBuilder.indicatorComputation()
+					.technology(react)
+					.indicatorRoute("log:just a test")
+					.build());
+		}
 		// When
 		Iterable<IndicatorComputation> allIndicatorComputations = tested.findAllPriorized();
 		List<IndicatorComputation> indicatorComputations = StreamSupport.stream(allIndicatorComputations.spliterator(), false)
